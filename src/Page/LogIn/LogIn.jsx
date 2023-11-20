f14362d1e5ba0405998d85e709a4f7a523d0f6a0
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-11-20 17:58:00 +0200

    add LogIn

---
+++ src/Page/LogIn/LogIn.jsx
@@ -0,0 +1,66 @@
+import { Button, Container } from '@mui/material';
+import { FieldEl, FormEl } from 'components/ContactForm/ContactForm.styled';
+import { Section } from 'components/Section/Section.styled';
+import { Formik } from 'formik';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { logIn } from 'redux/auth/auth-operations';
+
+export const LogIn = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = (values, actions) => {
+    dispatch(logIn(values));
+  };
+
+  return (
+    <main>
+      <Section>
+        <Container>
+          <h1 style={{ marginTop: '40px', textAlign: 'center', color: 'gray' }}>
+            Enter your data
+          </h1>
+          <Formik
+            initialValues={{ email: ``, password: '' }}
+            // initialValues={{ name: `${Math.ceil(Math.random()*100)}r@eeee.yyy`, email: `${Math.ceil(Math.random()*100)}r@eeee.yyy`, password: '12342343243' }}
+            onSubmit={handleSubmit}
+          >
+            <FormEl autoComplete="off">
+              <label htmlFor="email">E-mail</label>
+              <div>
+                <FieldEl
+                  // as={TextField}
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Enter e-mail ..."
+                  label="email"
+                  variant="outlined"
+                />
+
+                {/* <FormError name="name" /> */}
+              </div>
+              <label htmlFor="password">Password</label>
+              <div>
+                <FieldEl
+                  // as={TextField}
+                  type="text"
+                  id="password"
+                  name="password"
+                  placeholder="Enter password ..."
+                  label="password"
+                  variant="outlined"
+                />
+
+                {/* <FormError name="name" /> */}
+              </div>
+              <Button variant="contained" type="submit">
+                submit
+              </Button>
+            </FormEl>
+          </Formik>
+        </Container>
+      </Section>
+    </main>
+  );
+};

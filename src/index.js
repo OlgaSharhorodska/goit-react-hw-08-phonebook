93cb58460842b9ddf11244f6218f4d84cd8ae6ff
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-11-21 21:29:37 +0200

    add GlobalStyle

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from 'components/GlobalStyle';
+import { GlobalStyle } from 'GlobalStyle';
 import { Provider } from 'react-redux';
 import { persistor, store } from 'redux/store';
 import { BrowserRouter } from 'react-router-dom';
